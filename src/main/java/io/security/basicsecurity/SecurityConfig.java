549aa7007c13bbcbd455dee6f79ec4f22be7e224
seonongs <seonongs@gmail.com>
2022-06-10 17:18:35 +0900

    changed

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,17 +1,35 @@
 package io.security.basicsecurity;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
 @Configuration
 @EnableWebSecurity
+@Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated();
-        http.formLogin();
+        http.formLogin()
+//                .loginPage("/loginPage")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login")
+                .usernameParameter("userId")
+                .passwordParameter("passwd")
+                .loginProcessingUrl("/login_proc")
+
+                .successHandler((request, response, authentication) -> {
+                    log.info("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< authentication: " + authentication.getName());
+                    response.sendRedirect("/");
+                })
+                .failureHandler((request, response, exception) -> {
+                    log.info("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< exception: " + exception.getMessage());
+                    response.sendRedirect(("/login"));
+                })
+                .permitAll();
+
     }
 }
